9656379c08cfab6c24a51d56cb24cd756025eb2b
Andrea Barajas <108833574+AndreBarajas@users.noreply.github.com>
2023-02-12 14:34:36 -0600

    feat: adds some setle to modal

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -5,6 +5,14 @@ function EditModal({clickEdit, handleModal}) {
         <>{clickEdit && 
             <div className="overlay">
                 <div className="modalContainer">
+                    <p>Title</p>
+                    <input type='text'className="input"></input>
+                    <p>Description</p>
+                    <input type='text' className="input"></input>
+                    <p>Categorie</p>
+                    <input type='text' className="input"></input>
+                    <p>Image</p>
+                    <input className="input"></input>     
                     <button onClick={()=> handleModal(false)}>Cerrar</button>
                 </div>
             </div>
